a2067ed2a9559ec133bcdd3c367babc98a9f45bb
FBNdao <158055020+FBNdao@users.noreply.github.com>
2025-01-10 22:12:21 +0100

    Update Filtrer.ts en ajoutant la partie 2

--- Filtrer.ts
+++ Filtrer.ts
@@ -25,23 +25,23 @@ const users: User[] = [
   console.log(filtrerSelonNomEtAge(users, "John", 25));
   console.log(filtrerSelonNomEtAge(users, "John", 24));
 
+
+
 // partie 2 
 import * as fs from 'fs';
 
-function readUsersFromFile(filePath: string): User[] {
-  const data = fs.readFileSync(filePath, 'utf-8');
+function lireAPartirDUnFichier(fichier: string): User[] {
+  const data = fs.readFileSync(fichier, 'utf-8');
   return JSON.parse(data);
 }
 
-
-// partie 2
 function filtreP2(source: User[] | string, nom: string, age: number): User[] {
     var users: User[];
   
     if (Array.isArray(source)) {
       users = source;
     } else {
-      users = readUsersFromFile(source);
+      users = lireAPartirDUnFichier(source);
     }
   
     return users.filter(user =>
